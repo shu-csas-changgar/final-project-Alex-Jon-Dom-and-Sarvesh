ed856e6cd6ba39b67937f0c32d85204ca4c88b88
Alexander A Varghese <vargheal@shu.edu>
2019-04-11 01:10:07 -0400

    Added Some Components in AdminPage, Deleted BottomNavigationBar and added it to Home

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
-import { NavigationBar } from '../Components/NavigationBar';
+import { Nav, NavDropdown, Navbar, Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+    background-color: #222;
+  }
+
+  .navbar-brand, .navbar-nav .nav-link, .navbar-text, .form-label {
+    color: #C0C0C0;
+    
+    &:hover {
+      color: white;
+    }
+  }
+`
+
+export const AdminNavigation = () => ( 
+  <Styles>   
+    <Navbar>
+      <Navbar.Brand href="/">ABC Group</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>Signed in as: Alexander Varghese&nbsp;</Navbar.Text>
+      </Navbar.Collapse>
+      <Button href="/">Sign Out</Button>
+    </Navbar>
+    <Navbar expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+</Styles>
+)
 
 
 class AdminPage extends Component {
     render ()  {
         return (
-            <NavigationBar />
+          <React.Fragment>
+            <AdminNavigation />
+          </React.Fragment>
         );
     }
 }
-
-
-
-
 
 export default AdminPage;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import { NavigationBar } from '../Components/NavigationBar';
 import { Jumbotron } from '../Components/Jumbotron';
-import { BottomNavigationBar } from '../Components/BottomNavigationBar';
+import { Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+    background-color: #222;
+    margin-bottom:0;
+  }
+
+  .navbar-brand, .navbar-nav .nav-link {
+    color: #C0C0C0;
+    
+    &:hover {
+      color: white;
+    }
+  }
+  * {font-size: 15px;}
+ `
+
+ export const BottomNavigationBar = () => (
+    <Styles>
+      <Navbar expand="lg">
+      <Navbar.Brand href="/about">About</Navbar.Brand>
+      <Navbar.Brand>Address: 400 South Orange Ave, South Orange, NJ 07079</Navbar.Brand>  
+      <Navbar.Brand position="right">Contact: (973) 761-9000</Navbar.Brand>     
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Navbar>
+    </Styles>
+  )
 
 
 class Home extends Component {
